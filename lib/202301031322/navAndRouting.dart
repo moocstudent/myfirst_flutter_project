6b5505fe6667d4058696881cdd27748fb3743993
moocstudent <deadzq@qq.com>
2023-01-08 13:10:43 +0800

    done: widgets熟悉 navigator熟悉

--- lib/202301031322/navAndRouting.dart
+++ lib/202301031322/navAndRouting.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget{
       initialRoute: '/',
       routes: {
         '/':(context)=>HomePage(),
-        '/secondScreen':(context)=>SecondScreen(),
+        '/secondScreen':(context)=>SecondScreen(word: 'Hello', val: 4,),
       },
     );
   }
@@ -27,8 +27,14 @@ class HomePage extends StatelessWidget{
       body: Center(
         child: TextButton(
           child: Text("Click on Me"),
-          onPressed:(){
-            Navigator.pushNamed(context, "/secondScreen");
+          //To pass data to the back page, you need to put Navigator.push() method
+          //on "Click on Me" or any back buttons like below:
+          //传递参数到上一页时需要这样设置
+          onPressed:() async {
+            var backdata = await Navigator.push(context, MaterialPageRoute(builder:(context){
+              return SecondScreen(word: "Hello", val: 4);
+            }));
+            print(backdata);
           }
         )
       )
@@ -37,6 +43,9 @@ class HomePage extends StatelessWidget{
 }
 
 class SecondScreen extends StatelessWidget{
+  //声明变量为late也就是说明等到真正使用时再初始化
+  late String word;
+  late int val;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,14 +54,19 @@ class SecondScreen extends StatelessWidget{
       ),
       body: Center(
         child: TextButton(
-          child:Text("Go Back"),
+          //很明显$是引用变量
+          child:Text("Word:$word,Value:$val,- Go Back"),//go back button
           onPressed: (){
-            Navigator.pop(context);
+            //you need to put like widget.word on stateful widgets.
+            Navigator.pop(context,"returntext");
           },
         )
       )
     );
   }
+
+  //构造器参数使用{}内的参数在穿参时要声明参数名
+  SecondScreen({required this.word,required this.val});
 
 }
 
